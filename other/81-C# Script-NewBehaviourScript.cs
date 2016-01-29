3bd8467102323a2eca610060b6567411056d789c
Xarkos <oscarabelda@gmail.com>
2016-01-30 00:25:04 +0100

    Añadido fichero New Behavior Script, sustituir en C:\Program Files\Unity\Editor\Data\Resources\ScriptTemplates

---
+++ other/81-C# Script-NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class #SCRIPTNAME# : MonoBehaviour {
+	// Variables publicas
+
+	// Variables privadas
+
+	// Metodos Awake, Start, Update....
+
+	// Use this for spawn this instance
+	void Awake(){
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Otros métodos publicos
+
+	// Otros metodos privados
+}
