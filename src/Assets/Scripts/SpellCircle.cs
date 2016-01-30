be293f5acd863dc2b7af57ad5e877236e2730013
Xarkos <oscarabelda@gmail.com>
2016-01-30 02:06:54 +0100

    Añadida la funcionalidad del círculo

---
+++ src/Assets/Scripts/SpellCircle.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpellCircle : MonoBehaviour {
+    // Constantes públicas
+    public const float RUNE_ALPHA_ACTIVE = 1f;
+    public const float RUNE_ALPHA_UNACTIVE = 0.5f;
+
+    // Variables publicas
+
+    // Variables privadas
+    static SpellCircle instance = null;
+    List<SpellRune.Rune> _spell = new List<SpellRune.Rune>();
+
+    // Metodos Awake, Start, Update....
+
+    // Use this for spawn this instance
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.BroadcastMessage("Available", true);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            gameObject.BroadcastMessage("Available", false);
+            CheckSpell();
+            _spell.Clear();
+            gameObject.SetActive(false);
+
+        }
+    }
+
+    // Otros métodos publicos
+    public static void AddRune(SpellRune.Rune rune)
+    {
+        instance._spell.Add(rune);
+    }
+
+    // Otros metodos privados
+    void CheckSpell()
+    {
+        string s = "";
+        foreach (SpellRune.Rune rune in _spell)
+        {
+            s += rune.ToString() + " - ";
+        }
+        Debug.Log("Spell is: " + s);
+
+    }
+}
---
+++ src/Assets/Scripts/SpellRune.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SpellRune : MonoBehaviour {
+    // Enumerado publico
+    public enum Rune
+    {
+        Rune1,Rune2,Rune3,Rune4,Rune5
+    }
+
+    // Variables publicas
+    public Rune rune;
+
+    // Variables privadas
+    Image m_sprite;
+    bool avialable = false;
+    bool send = false;
+
+    // Metodos Awake, Start, Update....
+
+    // Use this for spawn this instance
+    void Awake()
+    {
+        m_sprite = GetComponent<Image>();
+    }
+
+    // Otros métodos publicos
+
+    public void Available(bool available)
+    {
+        this.avialable = available;
+        if (!available)
+            Deactivate();
+    }
+
+    public void Activate()
+    {
+        if (avialable && !send)
+        {
+            Color c = m_sprite.color;
+            c.a = SpellCircle.RUNE_ALPHA_ACTIVE;
+            m_sprite.color = c;
+
+            SpellCircle.AddRune(rune);
+
+            send = true;
+        }
+    }
+
+    public void Deactivate()
+    {
+        Color c = m_sprite.color;
+        c.a = SpellCircle.RUNE_ALPHA_UNACTIVE;
+        m_sprite.color = c;
+
+        avialable = false;
+        send = false;
+    }
+
+    // Otros metodos privados
+}
