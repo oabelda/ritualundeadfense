8ab87c1cdbdf342628494aa1084e3da7dbcd1aca
Fijer <feralcaden@gmail.com>
2016-01-30 18:55:11 +0100

    Added lot of things related to main menu

---
+++ src/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+	// Variables publicas
+    public string nombreEscena;
+
+	// Variables privadas
+
+	// Metodos Awake, Start, Update....
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.LoadScene(nombreEscena);
+	}
+	
+	// Otros métodos publicos
+
+	// Otros metodos privados
+}
---
+++ src/Assets/Scripts/SpellBegin.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SpellBegin : MonoBehaviour {
+    // Variables publicas
+    public GameObject spellBegin;
+    public GameObject Image;
+    public GameObject Flecha;
+	// Metodos Awake, Start, Update....
+
+	// Use this for initialization
+	void Start () {
+        SpellCircle.LearnSpell(spellBegin);
+	}
+
+    // Otros métodos publicos
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Image.SetActive(false);
+            Flecha.SetActive(true);
+        }
+    }
+
+	// Otros metodos privados
+}
