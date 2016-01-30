f46391e97355359421c20a1459a1d66d6af3a92c
Alonso Romero <a.r.penalba@gmail.com>
2016-01-30 13:12:03 +0100

    Prefab y manager de la UI

---
+++ src/Assets/Scripts/UiHudManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UiHudManager : MonoBehaviour {
+    // Variables publicas
+
+    // Variables privadas
+    [SerializeField]
+    private GameObject _shopHud;
+    [SerializeField]
+    private GameObject _torre;
+	// Metodos Awake, Start, Update....
+
+	// Use this for spawn this instance
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Otros métodos publicos
+    public void ToogleShop()
+    {
+        _shopHud.SetActive(!_shopHud.activeInHierarchy);
+    }
+	// Otros metodos privados
+}
